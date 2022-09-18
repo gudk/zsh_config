a8c256482ba1ee79edc891ba42e20144fbbd4fbe
Gudk <gudk@qq.com>
2022-09-18 10:28:23 +0800

    add wezterm.lua

---
+++ wezterm.lua
@@ -0,0 +1,194 @@
+local function basename(s)
+	return string.gsub(s, "(.*[/\\])(.*)", "%2")
+end
+local wezterm = require("wezterm")
+local SOLID_LEFT_ARROW = utf8.char(0xe0ba)
+local SOLID_LEFT_MOST = utf8.char(0x2588)
+local SOLID_RIGHT_ARROW = utf8.char(0xe0bc)
+
+local ADMIN_ICON = utf8.char(0xf49c)
+
+local CMD_ICON = utf8.char(0xe62a)
+local SSH_ICON = "💻"
+local PS_ICON = utf8.char(0xe70f)
+local ELV_ICON = utf8.char(0xfc6f)
+local WSL_ICON = utf8.char(0xf83c)
+local YORI_ICON = utf8.char(0xf1d4)
+local NYA_ICON = utf8.char(0xf61a)
+
+local VIM_ICON = utf8.char(0xe62b)
+local PAGER_ICON = utf8.char(0xf718)
+local FUZZY_ICON = utf8.char(0xf0b0)
+local HOURGLASS_ICON = utf8.char(0xf252)
+local SUNGLASS_ICON = utf8.char(0xf9df)
+local APPLE_ICON ="⌘"
+
+local PYTHON_ICON = utf8.char(0xf820)
+local NODE_ICON = utf8.char(0xe74e)
+local DENO_ICON = utf8.char(0xe628)
+local LAMBDA_ICON = utf8.char(0xfb26)
+
+local SUP_IDX = {
+	"¹",
+	"²",
+	"³",
+	"⁴",
+	"⁵",
+	"⁶",
+	"⁷",
+	"⁸",
+	"⁹",
+	"¹⁰",
+	"¹¹",
+	"¹²",
+	"¹³",
+	"¹⁴",
+	"¹⁵",
+	"¹⁶",
+	"¹⁷",
+	"¹⁸",
+	"¹⁹",
+	"²⁰",
+}
+local SUB_IDX = {
+	"₁",
+	"₂",
+	"₃",
+	"₄",
+	"₅",
+	"₆",
+	"₇",
+	"₈",
+	"₉",
+	"₁₀",
+	"₁₁",
+	"₁₂",
+	"₁₃",
+	"₁₄",
+	"₁₅",
+	"₁₆",
+	"₁₇",
+	"₁₈",
+	"₁₉",
+	"₂₀",
+}
+
+
+local mouse_bindings = {
+	{
+		event = { Down = { streak = 1, button = "Right" } },
+		mods = "NONE",
+		action = wezterm.action({ PasteFrom = "Clipboard" }),
+	},
+	-- Change the default click behavior so that it only selects
+	-- text and doesn't open hyperlinks
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "NONE",
+		action = wezterm.action({ CompleteSelection = "PrimarySelection" }),
+	},
+	-- and make CTRL-Click open hyperlinks
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "CTRL",
+		action = "OpenLinkAtMouseCursor",
+	},
+}
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	local edge_background = "#121212"
+	local background = "#4E4E4E"
+	local foreground = "#1C1B19"
+	local dim_foreground = "#3A3A3A"
+
+	if tab.is_active then
+		background = "#FBB829"
+		foreground = "#1C1B19"
+	elseif hover then
+		background = "#FF8700"
+		foreground = "#1C1B19"
+	end
+
+	local edge_foreground = background
+	local process_name = tab.active_pane.foreground_process_name
+	local pane_title = tab.active_pane.title
+	local exec_name = basename(process_name):gsub("%.exe$", "")
+	local title_with_icon
+
+	if exec_name == "ssh" then
+		title_with_icon = SSH_ICON .. " SSH"
+	elseif exec_name == "pwsh" then
+		title_with_icon = PS_ICON .. " PS"
+	elseif exec_name == "cmd" then
+		title_with_icon = CMD_ICON .. " CMD"
+	elseif exec_name == "elvish" then
+		title_with_icon = ELV_ICON .. " Elvish"
+	elseif exec_name == "wsl" or exec_name == "wslhost" then
+		title_with_icon = WSL_ICON .. " WSL"
+	elseif exec_name == "vim" or exec_name == "nvim" or exec_name == "vi" then
+		title_with_icon = VIM_ICON .. " " .. exec_name:upper()
+	elseif exec_name == "python" or exec_name == "hiss" then
+		title_with_icon = PYTHON_ICON .. " " .. exec_name
+	elseif exec_name == "node" then
+		title_with_icon = NODE_ICON .. " " .. exec_name:upper()
+	elseif exec_name == "zsh" then
+		title_with_icon = APPLE_ICON .. " " .. exec_name:upper()
+	elseif exec_name == "bb" or exec_name == "cmd-clj" or exec_name == "janet" or exec_name == "hy" then
+		title_with_icon = LAMBDA_ICON .. " " .. exec_name:gsub("bb", "Babashka"):gsub("cmd%-clj", "Clojure")
+	else
+		title_with_icon = HOURGLASS_ICON .. " " .. exec_name
+	end
+	if pane_title:match("^Administrator: ") then
+		title_with_icon = title_with_icon .. " " .. ADMIN_ICON
+	end
+	local left_arrow = SOLID_LEFT_ARROW
+	if tab.tab_index == 0 then
+		left_arrow = SOLID_LEFT_MOST
+	end
+	local id = tostring(tab.tab_index + 1)
+	local pid = SUP_IDX[tab.active_pane.pane_index + 1]
+	local title = " " .. wezterm.truncate_right(title_with_icon, max_width) .. " "
+
+	return {
+		{ Attribute = { Intensity = "Bold" } },
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = left_arrow },
+		{ Background = { Color = background } },
+		{ Foreground = { Color = foreground } },
+		{ Text = id },
+		{ Text = title },
+		{ Foreground = { Color = dim_foreground } },
+		{ Text = pid },
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = SOLID_RIGHT_ARROW },
+		{ Attribute = { Intensity = "Normal" } },
+	}
+end)
+
+wezterm.on("update-right-status", function(window)
+	local date = wezterm.strftime("%Y-%m-%d %H:%M:%S ")
+	window:set_right_status(wezterm.format({
+		{ Text = date },
+	}))
+end)
+
+return {
+	set_environment_variables = {
+		PATH = wezterm.executable_dir .. ";" .. os.getenv("PATH"),
+	},
+	window_frame = window_frame, -- needed only if using fancy tab
+	window_background_opacity = 1,
+	mouse_bindings = mouse_bindings,
+	disable_default_key_bindings = true,
+	default_prog = default_prog,
+	font_size = 15.0,
+	font = wezterm.font("MesloLGM Nerd Font Mono"),
+	colors = {
+		tab_bar = {
+			background = TAB_BAR_BG,
+		},
+	},
+	text_background_opacity = 0.95,
+}
